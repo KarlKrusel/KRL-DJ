f0f4f8fae41d9f5ee3ca4abc77b4f1ee48973eef
KarlKrusel <krusel.karl@gmail.com>
2026-05-20 17:15:36 -0400

    Fix router basepath for GitHub Pages

--- src/router.tsx
+++ src/router.tsx
@@ -8,6 +8,7 @@ export const getRouter = () => {
   const router = createRouter({
     routeTree,
     context: { queryClient },
+    basepath: "/KRL-DJ/",
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
   });
